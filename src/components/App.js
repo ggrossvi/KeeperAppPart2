ceeeb234a7e61abfc59ba7a747f9a83cfdb0d5b5
ggrossvi <hello.gloria@gmail.com>
2022-07-03 12:32:31 -0700

    Hardcoded one note works

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,25 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+/*
+{
+  key: 1,
+  title: "Delegation",
+  content:
+    "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+},
+*/
+//console.log(note);
 
 function App() {
   return (
-    <div>
+    <div className="note">
       <Header />
-      <Note />
+      <Note
+        key="1"
+        title="Delegation"
+        description="Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+      />
       <Footer />
     </div>
   );
